bfb13354116bc7bcbe284d56a2a0752eca1450f9
Ligiane <ligianealzie25@gmail.com>
2021-09-29 15:23:15 -0300

    construtor

--- construtor.py
+++ construtor.py
@@ -1,4 +1,10 @@
 #usando o construtor list()
 
-fruta = list(('maça','uva','buriti','abacaba')) # o construto lis() cria uma lista
+#fruta = list(('maça','uva','buriti','abacaba')) # o construtor lis() cria uma lista
+#print(fruta)
+
+# adicionando um elemento ou uma lista em lista
+
+fruta = ['maça','uva','buriti','abacaba']
+fruta.append('laranja')
 print(fruta)
