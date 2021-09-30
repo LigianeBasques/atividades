5513d3f3e76ce1edf98e7120a840f98b2047fb7a
Ligiane <ligianealzie25@gmail.com>
2021-09-30 07:58:39 -0300

    Ligi

---
+++ ligi.py
@@ -0,0 +1,24 @@
+
+
+#Exercicos de Ligiane
+
+#Faça uma lista vazia e depois adione elementos a esta lista:
+#Depois faça uma 2ª lista e adicione os elementos desta lista 2ª na 1ª lista.
+#Coloque a posição de cada elemento das listas.
+# Coloque o nunero de elementos da lista
+
+
+
+frutas_amazonicas = []
+frutas_amazonicas.append('Buriti')
+frutas_amazonicas.append('Açai')
+frutas_amazonicas.append('abacaba')
+peixes_amazonicos = ['jaraqui','pacu','pirarucu','tucunaré']
+frutas_amazonicas.extend(peixes_amazonicos)
+#print(frutas_amazonicas[5])
+print(len(frutas_amazonicas))
+
+
+
+
+
