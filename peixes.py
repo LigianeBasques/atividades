653dd945261466b018b3de4ce8a86f288688aaeb
Ligiane <ligianealzie25@gmail.com>
2021-10-04 10:05:58 -0300

    peixes

---
+++ peixes.py
@@ -0,0 +1,10 @@
+# crie uma lista vazia, faça um laço for que percorra a lista de peixes amazonicos
+# e adicione os peixes um a um na lista em branco .
+lista = []
+peixes_amazonicos = ['pacu','jaraqui','tucunaré','pirarucu']
+
+for peixes in peixes_amazonicos:
+        lista.append(peixes)
+
+print(lista)
+
