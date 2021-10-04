e0c43c6be4ff14c32fcfbeb80a0f7b5a908a1dec
Ligiane <ligianealzie25@gmail.com>
2021-10-04 14:15:19 -0300

    exercicios lista

---
+++ ligi1.py
@@ -0,0 +1,8 @@
+# faça uma lista em branco, e coloque os elementos na lista e peça a entranda para o usuario
+
+lista = []
+entrada = input('Digite o nome de uma fruta:')
+lista.append(entrada)
+
+
+
