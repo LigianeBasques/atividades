400237c898903f82f6f0277878d661535f439068
Ligiane <ligianealzie25@gmail.com>
2021-10-04 14:47:45 -0300

    exercicios lista

---
+++ loopfor.py
@@ -0,0 +1,8 @@
+#Um loop para para iteração sobre uma sequência (ou é uma lista, uma tupla, um dicionário, um conjunto ou uma sequência).
+
+#Imprima cada fruta em uma lista de frutas:
+frutas = ['apple', 'banana', 'uva', 'orange', 'abacate','mamão', 'morango']
+#lista = []
+#for fruta in frutas:
+    #lista.append(fruta)
+#print(lista)
