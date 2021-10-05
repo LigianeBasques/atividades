370ebebace10cb1def94c6724624701020e0fd2b
Ligiane <ligianealzie25@gmail.com>
2021-10-05 15:37:52 -0300

    for

--- ligi1.py
+++ ligi1.py
@@ -1,9 +1,10 @@
-# faça uma lista em branco, e coloque os elementos na lista e peça a entranda para o usuario
+# faça uma lista em branco, e coloque os elementos na lista e peça a entranda para o usuário
 
 lista = []
-entrada = input('Digite o nome de uma fruta:')
 
-lista.append(entrada)
+for x in range(0,5):
+    entrada = input('Digite o nome de uma fruta:')
+    lista.append(entrada)
 print(lista)
 
 
