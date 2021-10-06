0a77d99c77cc561d0a4cc3cdb0ac3bf4d5a466e5
Ligiane <ligianealzie25@gmail.com>
2021-10-06 16:23:40 -0300

    while

--- while2.py
+++ while2.py
@@ -6,8 +6,13 @@
 #while serve para quando sei o limte e não sei
 
 
-n = 1
-while n != 0:
-    n = int(input('Digite um valor: '))
-print("Fim")
+#n = 1
+#while n != 0:
+    #n = int(input('Digite um valor: '))
+#print("Fim")
 
+r = 'S'
+while r  == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N] ')).upper()
+print('FIM')
