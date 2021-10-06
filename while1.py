99d179f964344930cf9184f3d9f67bc6f4482b4a
Ligiane <ligianealzie25@gmail.com>
2021-10-06 09:53:48 -0300

    while

---
+++ while1.py
@@ -0,0 +1,12 @@
+#Com o loop while podemos executar um conjunto de declarações, desde que uma condição seja verdadeira.
+#i = 1
+#while i < 6:
+    #print(i)
+    #i = i + 1
+
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i = i + 1
