b95046d77011d38a068bbde738f3d62e335ed087
Ligiane <ligianealzie25@gmail.com>
2021-10-06 16:02:46 -0300

    while

--- while2.py
+++ while2.py
@@ -1,7 +1,13 @@
 # o for só serve quando eu sei o limite
-for c in range(1,3)
-    n = int(input('Digite um valor:'))
-print('Fim')
+#for c in range(1,5):
+    #n = int(input('Digite um valor:'))
+#print('Fim')
 
 #while serve para quando sei o limte e não sei
+
+
+n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+print("Fim")
 
